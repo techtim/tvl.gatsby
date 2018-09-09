531e433eca45916712f0c42a8392bc103f791eb4
Anton Vasin <antonvasin@me.com>
2018-09-09 17:08:32 +0300

    Single file

--- src/contact/index.js
+++ src/contact/index.js
@@ -1,5 +1,5 @@
+const nodemailer = require('nodemailer')
 const { json, send, createError } = require('micro')
-const getMailer = require('./getMailer')
 
 const makeText = message => `
   Создано ${new Date().toLocaleTimeString('ru-RU')} MSK.
@@ -17,7 +17,28 @@ module.exports = async (req, res) => {
       throw createError(400, 'Invalid input')
     }
 
-    const transporter = getMailer()
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: 'h5ywljnueofsriws@ethereal.email',
+        pass: '8rHnGtpgYBzVfeG6XC',
+      },
+    })
+
+    // const transporter =
+    //   process.env.NODE_ENV === 'production'
+    //     ? nodemailer.createTransport({
+    //         /*TODO:add production config*/
+    //       })
+    //     : nodemailer.createTransport({
+    //         host: 'smtp.ethereal.email',
+    //         port: 587,
+    //         auth: {
+    //           user: 'h5ywljnueofsriws@ethereal.email',
+    //           pass: '8rHnGtpgYBzVfeG6XC',
+    //         },
+    //       })
 
     try {
       await transporter.sendMail({
