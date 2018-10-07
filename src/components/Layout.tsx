2724fb06df8707f6db9dd92c05b924f25c25284f
Anton Vasin <antonvasin@me.com>
2018-10-07 22:49:04 +0300

    Basic footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Provider } from 'rebass'
+import { Flex, Box, Provider, Text } from 'rebass'
 import styled, { injectGlobal } from 'styled-components'
 import 'typeface-maven-pro'
 import Header from '../components/Header'
@@ -22,18 +22,18 @@ injectGlobal`
 
 const Layout: React.SFC = ({ children }) => (
   <Provider theme={theme}>
-    <Box width="100%">
+    <Flex flexDirection="column" width="100%" style={{ minHeight: '100vh' }}>
       <Header />
       <Container pt={112} px={0} mx="auto">
         {children}
       </Container>
 
-      {/* <Box is="footer" width="100%" pt={1} pb={2} px={5}>
+      <Box is="footer" width="100%" pt={1} pb={2} px={5} mt="auto">
         <Text fontSize={0} color="gray">
           ⓒ TVL {new Date().getFullYear()}
         </Text>
-      </Box> */}
-    </Box>
+      </Box>
+    </Flex>
   </Provider>
 )
 export default Layout
